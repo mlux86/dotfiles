3e51220debe9dea8f34050e6b933acb3832cf379
Markus Lux <mlux@ml1.net>
2021-07-31 19:46:44 +0200

    Fix fish prompt.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,15 +1,15 @@
-function fish_git_if_exists
-    if type -q fish_git_prompt
-        fish_git_prompt
-    end
-end
-
 function fish_prompt
     if test -n "$SSH_TTY"
         echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
     end
 
-    echo -n (set_color blue)(prompt_pwd)(set_color aaa)(fish_git_if_exists)' '
+    echo -n (set_color blue)(prompt_pwd)
+
+    if type -q fish_git_prompt
+        echo -n (set_color aaa)(fish_git_prompt)
+    end
+
+    echo -n ' '
 
     set_color -o
     if test "$USER" = 'root'
@@ -18,3 +18,4 @@ function fish_prompt
     echo -n (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
     set_color normal
 end
+
