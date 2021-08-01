2a197ffd884a019c22d009d9e949904a0895f72c
Markus Lux <mlux@ml1.net>
2021-08-01 11:55:11 +0200

    Add git abbr.

--- fish/config.fish
+++ fish/config.fish
@@ -23,12 +23,13 @@ abbr -a auuu sudo apt-get dist-upgrade
 # git
 
 abbr -a g git
-abbr -a gf fit getch
+abbr -a gf git fetch
 abbr -a ga git add
 abbr -a gr git remove
 abbr -a gc git commit
 abbr -a gca git commit --amend
 abbr -a gs git status
+abbr -a gd git diff
 abbr -a gco git checkout
 abbr -a gu git restore --staged
 
