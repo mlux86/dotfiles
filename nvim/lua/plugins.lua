806fd22a825c43259367aad57fac5c4ddd862430
Markus Lux <markus.lux@qaware.de>
2025-03-30 15:18:08 +0200

    Update

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,10 +1,18 @@
 return {
-    { "nvim-lua/plenary.nvim" },
-    { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
-    { "kylechui/nvim-surround" , event = "VeryLazy", config = function()
+    {
+        "nvim-lua/plenary.nvim"
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" }
+    },
+    {
+        "kylechui/nvim-surround",
+        event = "VeryLazy",
+        config = function()
             require("nvim-surround").setup({
                 -- Configuration here, or leave empty to use defaults
             })
         end
-    }
+    },
 }
