b4fbd0976f0462004bb1632b7f64540ceb955390
Markus Lux <markus.lux@qaware.de>
2025-07-10 20:31:22 +0200

    Update

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -52,5 +52,12 @@ return {
     }, -- }}}
     { -- {{{ github/copilot.vim
         "github/copilot.vim",
+        config = function()
+            vim.g.copilot_no_tab_map = true
+            vim.keymap.set('i', '<M-CR>', 'copilot#Accept("")', {
+                expr = true,
+                replace_keycodes = false
+            })
+        end
     } -- }}}
 }
